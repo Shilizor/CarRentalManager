34aab5431c66bb0b9e053cb2f8807c34c50485d6
Антон Шипилов <shilizor@ya.ru>
2023-05-17 13:41:48 +0700

    RelayCommand

---
+++ CarRentalManager/Model/WindowService.cs
@@ -0,0 +1,30 @@
+﻿using CarRentalManager.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CarRentalManager.Model
+{
+    public class WindowService : IWindowService
+    {
+        public void CloseWindow()
+        {
+            // Get a reference to the current window
+            var window = Application.Current.Windows.OfType<Window>().SingleOrDefault(x => x.IsActive);
+
+            // Close the window
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
+
+        public void ShowWindow()
+        {
+        }
+    }
+}
